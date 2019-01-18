e62dd6615ec017ece7e7e06dcd359644cc9b3937
apollowebdesigns <andrewevans0@outlook.com>
2019-01-18 19:21:47 +0000

    bringing xmas to the project

--- xmas.py
+++ xmas.py
@@ -1,10 +1,11 @@
 from gpiozero import LEDBoard
 from gpiozero.tools import random_values
 from time import sleep
-tree = LEDBoard(*range(2,28),pwm=True)
 
 
 def light_up_xmas():
+    tree = LEDBoard(*range(2, 28), pwm=True)
+
     for led in tree:
         led.source_delay = 0.1
         led.source = random_values()
