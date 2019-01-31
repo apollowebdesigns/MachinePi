5f91f8d4469471b8ebedc19162ea6ebceeb83556
apollowebdesigns <andrewevans0@outlook.com>
2019-01-31 20:04:18 +0000

    added flask to the project

--- camera.py
+++ camera.py
@@ -14,9 +14,7 @@ import numpy as np
 from PIL import Image
 # from arminit import MoveArm
 from xmas import light_up_xmas
-from multiprocessing import Process
-
-p = Process(target=light_up_xmas)
+import threading
 
 # Load the model
 net = cv2.dnn.readNet('face-detection-adas-0001.xml', 'face-detection-adas-0001.bin')
@@ -85,7 +83,7 @@ class Camera(object):
 
                     if confidence > 0.5:
                         # light_up_xmas()
-                        p.start()
+                        threading.Thread(target='light_up_xmas').start()
                         cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
 
                 ret, jpeg = cv2.imencode('.jpg', image)
